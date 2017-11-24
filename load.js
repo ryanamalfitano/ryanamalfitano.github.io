29381e201874f6126456ef4f30df519b07eb3c3b
ryanamalfitano <ryan.amalfitano@gmail.com>
2017-11-24 04:09:52 -0500

    Portfolio 1.0
    Added Web and Game page content. Fixed some styling issues. New front page background. Removed old files. Improved file organization.

--- load.js
+++ load.js
@@ -1,1 +1,13 @@
 // stretch to browser size
+$(function(){
+	var elements = ["#frontpage", "#game-main", "#web-main"];
+	for (var i = 0; i < elements.length; i++) {
+		var $content = $(elements[i]);
+		var $window = $(window).on('resize', function(){
+			if ($content.height() < $(this).height()) {
+				var height = $(this).height();
+				$content.height(height);
+			}
+		}).trigger('resize'); //on page load
+	}
+});
